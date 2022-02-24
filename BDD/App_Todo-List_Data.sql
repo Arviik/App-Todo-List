d951172766d390e7abab0fd4bdce54a309a1eb53
Astralale <benzekkitalal@gmail.fr>
2022-02-24 17:24:22 +0100

    Ajout : Ajout de la base de donnée(MCD,MLD), Ajout de donnée

---
+++ BDD/App_Todo-List_Data.sql
@@ -0,0 +1,12 @@
+INSERT INTO `etat`(etat)
+VALUES ('en attente'),
+       ('à accepter'),
+       ('à finir'),
+       ('en retard'),
+       ('expiré'),
+       ('terminé'),
+       ('refusé');
+
+INSERT INTO `type`(libelle)
+VALUES ('développement'),
+       ('édition');
