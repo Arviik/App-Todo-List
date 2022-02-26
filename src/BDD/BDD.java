527e0d9e302cf81c21df0f18e2fc6f3b0893bbc4
Arvik <ugo.bercegeay@gmail.com>
2022-02-26 03:14:22 +0100

    Ajout : ajout de la class BDD

---
+++ src/BDD/BDD.java
@@ -0,0 +1,20 @@
+package BDD;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class BDD {
+    private final Connection cnx;
+
+    public BDD() throws SQLException {
+        cnx = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/app_todo-list?serverTimezone=UTC",
+                "root",
+                "");
+    }
+
+    public Connection getCnx(){
+        return cnx;
+    }
+}
